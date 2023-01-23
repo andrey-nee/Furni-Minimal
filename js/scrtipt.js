d09929b0f886cf07b7719a7b3adf0768d318443d
Андрей <andrey.nee91@gmail.com>
2023-01-23 18:53:21 +0500

    Редактирует скрипты меню-бургер

--- js/scrtipt.js
+++ js/scrtipt.js
@@ -1,10 +1,35 @@
-var burger = document.querySelector('.main-header__menu-burger')
-var nav = document.querySelector('.main-navigation')
-var bosy = document.getElementsByTagName('body')
+const body = document.querySelector('.page-body')
+const burger = document.querySelector('.main-header__menu-burger')
+const burgerNav = document.querySelector('.main-navigation')
+const burgerLink = document.querySelector('.main-navigation__link')
 
 // Меню бургер для мобилок
 burger.addEventListener('click', function () {
+  console.log('Открываем-Закрываем меню-бургер');
   burger.classList.toggle('active');
-  nav.classList.toggle('active');
-  body.classList.toggle('lock');
+  burgerNav.classList.toggle('active');
+  console.log('Выключаем прокрутку сайта');
+  body.classList.toggle('lock'); // Запрещаем прокрутку всего сайта при открытом меню-бургере
+});
+
+// Закрываем меню-бургер при клике на ссылку в навигации
+burgerLink.addEventListener('click', function () {
+  if (burger.classList.contains('active')) {
+    console.log('Закрываем меню-бургер');
+    console.log('Включаем прокрутку сайта');
+    burger.classList.remove('active');
+    burgerNav.classList.remove('active');
+    body.classList.remove('lock'); // Снимаем запрет на прокрутку всего сайта при открытом меню-бургере
+  }
+});
+
+// Закрываем меню-бургер при клике за его пределами
+document.addEventListener( 'click', (e) => {
+  const withinBoundaries = e.composedPath().includes(burger);
+  if ( ! withinBoundaries ) {
+    console.log('Клик вне меню-бургер');
+    burger.classList.remove('active');
+    burgerNav.classList.remove('active');
+    body.classList.remove('lock'); // Снимаем запрет на прокрутку всего сайта при открытом меню-бургере
+  }
 });
